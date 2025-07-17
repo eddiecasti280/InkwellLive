1f69edf4c54ab2ab185b0253a9ba68c6ff655fbc
Eddie Castillo <46185445+eddiecasti280@users.noreply.github.com>
2025-07-17 15:30:26 -0500

    Fix: Add missing routes for stories, reading view, and new writing pages

--- client/App.tsx
+++ client/App.tsx
@@ -26,6 +26,9 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/stories" element={<Stories />} />
+            <Route path="/stories/:id" element={<ReadingView />} />
+            <Route path="/new-writing" element={<NewWriting />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
